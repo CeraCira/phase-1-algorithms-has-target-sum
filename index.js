b8ec31a7799f658954ea733b358c9c450de35618
Waithera Munderu <wmunderu@gmail.com>
2022-06-19 08:37:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // for (const num1 of array) {
+  //   for (const num2 of array) {
+  //     let totalSumOfNumbers = num1 + num2;
+  //     if (totalSumOfNumbers === target) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
+  // return false;
+  for (let i = 0; i < array.length; i++) {
+    const number = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === number) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n2) -nested loop
 */
 
 /* 
@@ -12,6 +35,7 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+   Returns true if any set of two numbers sums up to equal the target
 */
 
 // You can run `node index.js` to view these console logs
